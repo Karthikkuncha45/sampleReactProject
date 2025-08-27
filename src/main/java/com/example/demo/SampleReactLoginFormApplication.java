8450386d970891c3d246c90e2449ccfd8f2953cf
Kuncha Karthik <kunchakarthik0@gmail.com>
2025-08-27 15:21:59 +0530

    initial commit

---
+++ src/main/java/com/example/demo/SampleReactLoginFormApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleReactLoginFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleReactLoginFormApplication.class, args);
+	}
+
+}
